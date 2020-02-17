c074ce2d94f7f25c9b644edede4475970a1bc9a1
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-17 11:28:56 +0100

    Create mssql.sh

---
+++ mssql.sh
@@ -0,0 +1,4 @@
+kubectl create -f https://raw.githubusercontent.com/MateuszHub/kubeadm/master/service-account.yaml
+kubectl create -f https://raw.githubusercontent.com/MateuszHub/kubeadm/master/service.yaml
+kubectl create -f https://raw.githubusercontent.com/MateuszHub/kubeadm/master/sqlconfig.yaml
+kubectl create -f https://raw.githubusercontent.com/MateuszHub/kubeadm/master/mssql.yaml
