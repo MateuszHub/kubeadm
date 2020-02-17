355fd397769a64bab06207ee468657f0e52ea563
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-17 23:42:34 +0100

    Create startWorker.sh

---
+++ startWorker.sh
@@ -0,0 +1,13 @@
+curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/ports.sh
+curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/add.sh
+curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/docker.sh
+curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/installDocker.sh
+curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/setup.sh
+curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/clean.sh
+curl -O https://raw.githubusercontent.com/MateuszHub/key/master/output.txt
+chmod +x ports.sh add.sh docker.sh installDocker.sh setup.sh clean.sh output.txt
+./installDocker.sh
+./setup.sh
+sudo ./docker.sh
+./add.sh
+sudo ./ports.sh
