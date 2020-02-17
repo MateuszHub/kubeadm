282a908928b6186d6a3b6efa40c0d488099519f8
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-17 23:04:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
 sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 sudo swapoff -a  
-sudo sed -i '/ swap / s/^/#/' /etc/fstab
+sudo sed -i "/ swap / s/^/#/" /etc/fstab
 sudo apt install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
