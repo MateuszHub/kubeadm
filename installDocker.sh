f9ca850858429f917e7809078e4c8b88f283ef5c
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-17 19:41:46 +0100

    Update installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -9,6 +9,6 @@ sudo apt-get install \
  sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
+   stable edge test"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
