d001ea1f63d5688b9ebc655d7e1154ebf1baf955
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-17 09:08:49 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+sudo apt-get install -y iptables arptables ebtables
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
+sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+sudo swapoff -a  
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+sudo apt install docker.io -y  
+sudo systemctl start docker  
+sudo systemctl enable docker 
+sudo apt install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt update  
+sudo apt install -y kubeadm kubelet kubectl  
+
