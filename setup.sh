aa294e36682f5c1c52d46af7704d3a0db12cd665
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-17 23:47:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,5 +12,6 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt update  
+sudo apt-get install nfs-common
 sudo apt install -y kubeadm kubelet kubectl  
 
