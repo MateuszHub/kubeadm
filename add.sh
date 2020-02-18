41e705b53c775b70f6aec8ea8608f495dff53025
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-18 13:52:58 +0100

    Update add.sh

--- add.sh
+++ add.sh
@@ -4,4 +4,5 @@ docker run --name enable_lio                  \
            --cap-add=SYS_ADMIN                \
            -v /lib/modules:/lib/modules       \
            -v /sys:/sys:rshared               \
+           -v /var/opt/mssql:/var/opt/mssql:rshared               \
            storageos/init:0.2
