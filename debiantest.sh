810681278ebc2c3373f5480c159c250e18d48de1
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-18 15:19:44 +0100

    Create debiantest.sh

---
+++ debiantest.sh
@@ -0,0 +1,2 @@
+kubectl -f https://raw.githubusercontent.com/storageos/deploy/master/k8s/examples/pvc.yaml
+kubectl -f https://raw.githubusercontent.com/storageos/deploy/master/k8s/examples/debian-pvc.yaml
