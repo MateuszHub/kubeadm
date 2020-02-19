bd5c01d77a9c224b6c663afcf98506a3e86adeae
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-19 01:44:33 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,5 +1,4 @@
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 > output.txt
 mkdir -p $HOME/.kube  
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config  
-sudo chown $(id -u):$(id -g) $HOME/.kube/config  
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml  
+sudo chown $(id -u):$(id -g) $HOME/.kube/config    
