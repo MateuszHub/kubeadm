e7c4609cc5e6020928a646d7952c754bf98681d2
MateuszHub <mateusz.n.mail@gmail.com>
2020-02-19 01:46:13 +0100

    Update startWorker.sh

--- startWorker.sh
+++ startWorker.sh
@@ -9,6 +9,8 @@ curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/master.sh
 curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/push.sh
 curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/storageOs.sh
 curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/mssql.sh
+curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/flannel.sh
+curl -O https://raw.githubusercontent.com/MateuszHub/kubeadm/master/pull.sh
 chmod +x ports.sh add.sh docker.sh installDocker.sh setup.sh clean.sh output.txt
 ./installDocker.sh
 ./setup.sh
